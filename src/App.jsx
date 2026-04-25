5c3a14201602e0c000c8a118ac913d8cad9fb98a
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-04-25 18:39:23 +0100

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ProductPreview from "./components/ProductPreview";
+
+export default function App() {
+  return (
+    <main>
+      <ProductPreview />
+    </main>
+  );
+}
---
+++ src/components/ProductPreview.jsx
@@ -0,0 +1,7 @@
+export default function ProductPreview() {
+  return (
+    <div>
+      <h1>Product preview card</h1>
+    </div>
+  );
+}
