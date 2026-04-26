3b7f65894d238a8465ec920a869a1c93d236cdc7
TheDeveloper <171641141+JusticeCodes00@users.noreply.github.com>
2026-04-26 11:44:51 +0100

    feat: add JSX for product preview component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import ProductPreview from "./components/ProductPreview";
+import thumbnailDesktop from "./assets/images/image-product-desktop.jpg";
+import thumbnailMobile from "./assets/images/image-product-mobile.jpg";
+
+const productData = {
+  category: "Perfume",
+  name: "Gabrielle Essence Eau De Parfum",
+  description:
+    "A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.",
+  price: {
+    current: 149.99,
+    original: 169.99,
+    currency: "$",
+  },
+  image: {
+    thumbnailDesktop,
+    thumbnailMobile,
+    alt: "Gabrielle Essence Eau De Parfum perfume bottle on a white surface with green leaves",
+  },
+};
 
 export default function App() {
   return (
-    <main>
-      <ProductPreview />
+    <main className="flex items-center justify-center bg-cream min-h-screen">
+      <ProductPreview {...productData} />
     </main>
   );
 }
--- src/components/ProductPreview.jsx
+++ src/components/ProductPreview.jsx
@@ -1,7 +1,31 @@
-export default function ProductPreview() {
+import cartIcon from "../assets/images/icon-cart.svg";
+
+export default function ProductPreview(props) {
+  const { image, category, title, description, price } = props;
+
   return (
-    <div>
-      <h1>Product preview card</h1>
-    </div>
+    <article className="bg-white">
+      <picture>
+        <source media="(width >= 1440px)" srcSet={image.thumbnailDesktop} />
+        <source media="(width >= 375px)" srcSet={image.thumbnailMobile} />
+        <img src={image.thumbnailDesktop} alt={image.alt} />
+      </picture>
+      <p>{category}</p>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <p>
+        <span>
+          {price.currency}
+          {price.current}
+        </span>{" "}
+        <span>{price.original}</span>
+      </p>
+      <p>
+        <button>
+          <img src={cartIcon} alt="" />
+          <span>Add to Cart</span>
+        </button>
+      </p>
+    </article>
   );
 }
